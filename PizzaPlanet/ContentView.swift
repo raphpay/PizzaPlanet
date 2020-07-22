58cb0188fd28fe9109cd59de50b8507bac691a48
Raphael Payet <raphpay.dev@gmail.com>
2020-07-22 19:01:52 +0200

    Initial Commit

---
+++ PizzaPlanet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaPlanet
+//
+//  Created by Raphaël Payet on 22/07/2020.
+//  Copyright © 2020 Oriapy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
